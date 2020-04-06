a4432ca7ed9b7df14641509b2d488b8be3856cf5
prashi23 <prashidoval23@gmail.com>
2020-04-06 10:06:50 +0530

    added print function

--- main.asm
+++ main.asm
@@ -1,12 +1,25 @@
-MOV AH, 0X0E
-;MOVE CURSOR FORWARD
+[org 0x7c00]
 
-MOV AL, 'X'
-INT 0X10
-;BIOS INTERRUPT TO PRINT TO SCREEN
-
+MOV si, STR
+call printf
 ;JUMP TO CURRENT LOCATION
 JMP $
+printf:
+    PUSHA  ;to print everything on stack and pusha works only on 16bit mode
+    str_loop:
+        MOV al, [si]
+        cmp al, 0
+        jne print_char
+        popa
+        ret
+
+    print_char:
+        mov ah, 0x0e
+        int 0x10
+        add si, 1
+        jmp str_loop
+
+STR: db 'hello world', 0
 
 ;PADDING AND MAGIC NUMBER
 TIMES 510- ($- $$) DB 0
